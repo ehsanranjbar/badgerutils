5b13be0c83b95abcc10241ea9b13830d3b2c3ad8
ehsanranjbar <ehsan.ranjbar.2000@gmail.com>
2024-08-19 13:57:46 +0000

    Optimize use of reflections in object store

--- store/object/store.go
+++ store/object/store.go
@@ -1,11 +1,8 @@
 package object
 
 import (
-	"encoding"
-	"encoding/binary"
 	"errors"
 	"fmt"
-	"reflect"
 
 	"github.com/dgraph-io/badger/v4"
 	"github.com/ehsanranjbar/badgerutils"
@@ -33,6 +30,7 @@ type Store[K any, V any, PV interface {
 	vstore  *sstore.Store[V, PV]
 	istore  badgerutils.BadgerStore
 	indexer Indexer[V]
+	ks      keySerializer[K]
 }
 
 // New creates a new ObjectStore.
@@ -47,12 +45,13 @@ func New[K any, V any, PV interface {
 		vstore:  sstore.New[V, PV](pstore.New(base, vstorePrefix)),
 		istore:  pstore.New(base, istorePrefix),
 		indexer: indexer,
+		ks:      newKeySerializer[K](),
 	}
 }
 
 // Delete deletes an object along with all it's auxiliary references (i.e. secondary indexes).
 func (s *Store[K, V, PV]) Delete(key K) error {
-	keyBz := anyToBytes(key)
+	keyBz := s.ks.Marshal(key)
 
 	err := s.deleteRefs(keyBz)
 	if err != nil {
@@ -65,43 +64,6 @@ func (s *Store[K, V, PV]) Delete(key K) error {
 	}
 
 	return nil
-}
-
-func anyToBytes(v any) []byte {
-	switch v := v.(type) {
-	case int:
-		return binary.LittleEndian.AppendUint64(nil, uint64(v))
-	case uint:
-		return binary.LittleEndian.AppendUint64(nil, uint64(v))
-	case int8:
-		return []byte{byte(v)}
-	case uint8:
-		return []byte{byte(v)}
-	case int16:
-		return binary.LittleEndian.AppendUint16(nil, uint16(v))
-	case uint16:
-		return binary.LittleEndian.AppendUint16(nil, v)
-	case int32:
-		return binary.LittleEndian.AppendUint32(nil, uint32(v))
-	case uint32:
-		return binary.LittleEndian.AppendUint32(nil, v)
-	case int64:
-		return binary.LittleEndian.AppendUint64(nil, uint64(v))
-	case uint64:
-		return binary.LittleEndian.AppendUint64(nil, v)
-	case string:
-		return []byte(v)
-	case []byte:
-		return v
-	case encoding.BinaryMarshaler:
-		bz, err := v.MarshalBinary()
-		if err != nil {
-			panic(fmt.Sprintf("failed to marshal binary: %v", err))
-		}
-		return bz
-	default:
-		panic(fmt.Sprintf("unsupported type: %T", v))
-	}
 }
 
 func (s *Store[K, V, PV]) deleteRefs(key []byte) error {
@@ -124,7 +86,7 @@ func (s *Store[K, V, PV]) deleteRefs(key []byte) error {
 
 // Get gets an object given it's key.
 func (s *Store[K, V, PV]) Get(key K) (*V, error) {
-	keyBz := anyToBytes(key)
+	keyBz := s.ks.Marshal(key)
 
 	return s.vstore.Get(keyBz)
 }
@@ -151,41 +113,14 @@ func (s *Store[K, V, PV]) NewRefIterator(index string, opts badger.IteratorOptio
 	rs := refstore.New(pstore.New(s.istore, []byte(index)))
 
 	return iters.Map(rs.NewIterator(opts), func(key []byte, _ *badger.Item) (K, error) {
-		return bytesToAny[K](key), nil
+		return s.ks.Unmarshal(key), nil
 	})
-}
-
-func bytesToAny[T any](bz []byte) (v T) {
-	rv := reflect.ValueOf(&v).Elem()
-	switch rv.Kind() {
-	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		rv.SetInt(int64(binary.LittleEndian.Uint64(bz)))
-	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-		rv.SetUint(binary.LittleEndian.Uint64(bz))
-	case reflect.String:
-		rv.SetString(string(bz))
-	case reflect.Slice:
-		if rv.Type().Elem().Kind() == reflect.Uint8 {
-			rv.SetBytes(bz)
-		}
-	case reflect.Ptr:
-		if rv.Type().Elem().Implements(reflect.TypeOf((*encoding.BinaryUnmarshaler)(nil)).Elem()) {
-			err := any(&v).(encoding.BinaryUnmarshaler).UnmarshalBinary(bz)
-			if err != nil {
-				panic(fmt.Sprintf("failed to unmarshal binary: %v", err))
-			}
-		}
-	default:
-		panic(fmt.Sprintf("unsupported type: %T", v))
-	}
-
-	return v
 }
 
 // Set inserts the object into the store as a new object or updates an existing object
 // along with inserting/updating all it's auxiliary references (i.e. secondary indexes).
 func (s *Store[K, V, PV]) Set(key K, obj *V) error {
-	keyBz := anyToBytes(key)
+	keyBz := s.ks.Marshal(key)
 
 	err := s.deleteRefs(keyBz)
 	if err != nil && !errors.Is(err, badger.ErrKeyNotFound) {
