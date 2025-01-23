f49896bef5413e1b13c6685c87ff91aef23c2a3a
ehsanranjbar <ehsan.ranjbar.2000@gmail.com>
2025-01-23 13:51:14 +0000

    Add Decrement function to lex package for lexicographical order

--- codec/lex/util.go
+++ codec/lex/util.go
@@ -20,3 +20,14 @@ func Increment(b []byte) []byte {
 	}
 	return append(bytes.Repeat([]byte{0xff}, len(b)), 0x01)
 }
+
+// Decrement decrements the given byte slice so that it would be the previous in lexicographical order.
+func Decrement(b []byte) []byte {
+	for i := len(b) - 1; i >= 0; i-- {
+		b[i]--
+		if b[i] < 0xff {
+			return b
+		}
+	}
+	return bytes.Repeat([]byte{0x00}, len(b))
+}
